7430d28125e86b18c51130cda1988dca87f5480b
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2024-03-04 11:30:14 +0800

    update TelegramText:implement auto escaping text

--- src/core/telegram-text.js
+++ src/core/telegram-text.js
@@ -12,8 +12,17 @@ class TelegramText {
         return this.$text;
     }
 
-    addText(text) {
-        this.$text = this.$text + `${ text }`;
+    escapeText(text) {
+        const escapedChars = ["_", "*", "`", "["];
+        const regex = new RegExp(`[${ escapedChars.join("\\") }]`, "g");
+        return text.replace(regex, '\\$&');
+    }
+
+    addText(text, escapeText = true) {
+        if(escapeText)
+            this.$text = this.$text + this.escapeText(`${ text }`);
+        else
+            this.$text = this.$text + `${ text }`;
         return this;
     }
 
@@ -24,21 +33,21 @@ class TelegramText {
         return this;
     }
 
-    startBold() { return this.addText("*"); }
-    endBold() { return this.addText("*"); }
-    addBold(text) { return this.startBold().addText(text).endBold(); }
+    startBold() { return this.addText("*", false); }
+    endBold() { return this.addText("*", false); }
+    addBold(text, escapeText = true) { return this.startBold().addText(text, escapeText).endBold(); }
 
-    startItalic() { return this.addText("___"); }
-    endItalic() { return this.addText("___"); }
-    addItalic(text) { return this.startItalic().addText(text).endItalic(); }
+    startItalic() { return this.addText("___", false); }
+    endItalic() { return this.addText("___", false); }
+    addItalic(text, escapeText = true) { return this.startItalic().addText(text, escapeText).endItalic(); }
 
-    startStrike() { return this.addText("~~"); }
-    endStrike() { return this.addText("~~"); }
-    addStrike(text) { return this.startStrike().addText(text).endStrike(); }
+    startStrike() { return this.addText("~~", false); }
+    endStrike() { return this.addText("~~", false); }
+    addStrike(text, escapeText = true) { return this.startStrike().addText(text, escapeText).endStrike(); }
 
-    startCode() { return this.addText("```").addLine(); }
-    endCode() { return this.addText("```"); }
-    addCode(text) { return this.startCode().addText(text).endCode(); }
+    startCode() { return this.addText("```", false).addLine(); }
+    endCode() { return this.addText("```", false); }
+    addCode(text, escapeText = true) { return this.startCode().addText(text, escapeText).endCode(); }
 
     addSpace(count = 1) {
         for(let i=1; i<=count; i++) {
@@ -50,15 +59,15 @@ class TelegramText {
     addTabspace() { return this.addSpace(8); }
 
     addUrl(url, text) {
-        return this.addText(`[${ text }](${ url })`);
+        return this.addText(`[${ text }](${ url })`, false);
     }
 
     addMentionByUsername(username) {
-        return this.addText(`[@${ username }](https://t.me/${ username })`);
+        return this.addText(`[@${ username }](https://t.me/${ username })`, false);
     }
 
     addMentionByUserId(userId, name) {
-        return this.addText(`[${ name }](tg://user?id=${ userId })`);
+        return this.addText(`[${ name }](tg://user?id=${ userId })`, false);
     }
 
 }
